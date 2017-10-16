5d920464be53a5c9df494f38b0aeb72b050d23f7
Valentin Niess <niess@in2p3.fr>
2017-10-16 13:01:44 +0200

    Configure the build environement in the setup.py script.

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import os
 from distutils.core import setup, Extension
 
+# Check the build environment.
+deps_dir = os.getenv("DEPS_DIR")
+if not deps_dir: deps_dir = "deps"
+lib_dir = os.getenv("LIB_DIR")
+if lib_dir:
+    if lib_dir.endswith("python"): lib_dir = "/".join(lib_dir.split("/")[:-1])
+else: lib_dir = "deps/turtle/lib"
+
+# Configure the extension module.
 module = Extension("grand_tour",
-                    include_dirs = ["deps/turtle/include"],
+                    include_dirs = [os.path.join(deps_dir, "turtle",
+                                                 "include")],
                     libraries = ["turtle"],
-                    library_dirs = ["deps/turtle/lib"],
+                    library_dirs = [lib_dir,],
                     sources = ["src/grand-tour.c"])
 
 setup(name = "grand_tour",
